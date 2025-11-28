234832ce8ac81a204664a9065385f5bfeec31a12
LeHuyVuu <lehuyvuok@gmail.com>
2025-11-28 18:44:03 +0700

    feat: add eslint and typescript configuration options in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Allow service worker to be served
